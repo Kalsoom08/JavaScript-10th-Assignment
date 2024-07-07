7bc4cf1555015127aa310ef51c27e63ebd718c17
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-07-07 22:54:36 +0500

    Class- Task

---
+++ script.js
@@ -0,0 +1,64 @@
+//Find Smallest Number
+// var arr = [];
+//     for(var i = 0; i < 5; i++){
+//         var enterNumber = +prompt("Enter Numbers In Array");
+//         arr.push(enterNumber); 
+//     }
+//     var smallestNumber = arr[0];
+//     for(var i = 0; i < 5; i++){
+//         if(arr[i] < smallestNumber){
+//             smallestNumber = arr[i];
+//         }
+//     }
+//     console.log(`Array ${arr}`)
+//     console.log(`The Smallest Number is ${smallestNumber}`) ;
+
+
+
+//Reverse Array
+// var number = [];
+//     for(var i = 0; i < 5; i++){
+//         var enterNumber = +prompt("Enter Numbers In Array");
+//         number.push(enterNumber); 
+//     }
+// reverseNumber = ""; 
+// for(var i = number.length-1; i >= 0; i--){
+    
+//  var reverseNumber = reverseNumber + number[i] 
+// }
+// console.log(`Array Before Reverse ${number}  `)
+// console.log(`After Reverse is ${reverseNumber}`)
+
+
+
+//Find Average 
+// var arr = [];
+// var sum = 0;
+// var avg = [];
+//  for(var i = 0 ; i < 4; i++){
+//      var enterNumber = +prompt("Enter Numbers");
+//      arr.push(enterNumber); 
+//  }
+//  for (let i = 0; i < 4; i++) {
+//      sum = sum + arr[i]; 
+//   avg = sum / arr.length;
+//  }
+//  console.log(`Sum = ${sum}`)
+//  console.log(`Average = ${avg}`);
+
+
+
+//Celsius To Fahrenheit 
+// var celsius = +prompt("Enter Temprature in Celsius")
+// var fahrenheit = (celsius * 9 / 5)  + 32;
+// console.log(`0 Celsuis = 32 Fahrenheit`);
+// console.log(`So ${celsius} Celsuis = ${fahrenheit} Fahrenheit` );
+
+
+
+
+//Fahrenheit to Celsius
+// var fahrenheit = +prompt("Enter Temperature in Fahrenheit")
+// var celsius = (fahrenheit - 32) * 5 / 9;
+// console.log(`32 Fahrenheit = 0 Celsuis`);
+// console.log(`So  ${fahrenheit} Fahrenheit = ${celsius} Celsuis` );
